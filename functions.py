f0fe4ca3661abdbbd3f3a07ed700a7c64d8b9159
Sander-317 <sander.van.meerveld@gmail.com>
2023-05-02 15:37:25 +0200

    addad write to file function

--- functions.py
+++ functions.py
@@ -1,15 +1,20 @@
 import csv
 
-def write_to_file(action, product_name, price, expiration_date,){
+
+def write_to_file(
+    action,
+    product_name,
+    price,
+    expiration_date,
+):
     to_add = {
         "action": action,
         "product_name": product_name,
         "price": price,
-        "expiration_date": expiration_date,}
+        "expiration_date": expiration_date,
+    }
 
-
-    with open("csv_rich_test/contacts.csv", "a", newline="") as new_file:
-            fieldnames = ["action", "product_name", "price", "expiration_date"]
-            csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames)
-            csv_writer.writerow(to_add)
-}
+    with open("data/inventory.csv", "a", newline="") as new_file:
+        fieldnames = ["action", "product_name", "price", "expiration_date"]
+        csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames)
+        csv_writer.writerow(to_add)
